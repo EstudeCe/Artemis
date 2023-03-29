0e96232acf65f72d1b1986082e1e5633b37d12a7
Vitor Sousa <v.sousa.cf@gmail.com>
2023-03-29 19:59:21 -0300

    Material do HTML

--- src/script.js
+++ src/script.js
@@ -1,23 +1,55 @@
 const button = document.getElementById("create-post");
 const containerPosts = document.getElementById("container");
 
+const posts = [
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: "Post sobre a importancia do treinamento de força",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erostellus, malesuada et velit iblandit molestie dolor.",
+    image: "https://picsum.photos/320/200",
+  },
+];
+
 const text = "Esse é o primeiro post utilizando template string";
 
 function handleClickButton() {
-  containerPosts.innerHTML += `<div class="container-post">
-  <img src="https://picsum.photos/320/200" alt="" />
+  const post = createPost(
+    Math.random(),
+    "https://picsum.photos/320/200",
+    "",
+    ""
+  );
+
+  containerPosts.innerHTML += post;
+}
+
+function createPost(id, image, title, description) {
+  const post = `<div class="container-post" id="${id}" >
+  <img src="${image}" alt="" />
   <div class="content-post">
     <h3>
-      ${text}
+      ${title}
     </h3>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros
-      tellus, malesuada et velit iblandit molestie dolor.
+      ${description}
     </p>
     <small>13/03/2023</small>
     <button>Ler post</button>
   </div>
 </div>`;
+
+  return post;
 }
+
+function addPost() {
+  const postsFormated = posts.map((post) => {
+    return createPost(post.id, post.image, post.title, post.description);
+  });
+
+  containerPosts.innerHTML = postsFormated;
+}
+
+addPost();
 
 button.addEventListener("click", handleClickButton);
---
+++ task/task003.md
@@ -0,0 +1,15 @@
+## Task 003
+
+### Situação atual:
+
+- Quando clicamos no button de criar post, hoje adicionamos um post estático que foi criado como exemplo. Porém, esse é apenas o post inicial.
+
+### Como deveria estar:
+
+- Você vai criar um post aleatório, com informações aleatórias dentro da function handleClickButton.
+- Depois disso, vai adicionar esse post no array de posts, utilizando um método de manipulação de array.
+- E por último, renderizar o post novo em tela
+
+### Qual o nome da branch criada ?
+
+- Defina um nome para branch
